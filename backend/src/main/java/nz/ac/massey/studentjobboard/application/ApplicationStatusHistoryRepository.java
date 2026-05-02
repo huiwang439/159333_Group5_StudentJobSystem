26d87bcd37d286a373556d3f3fab0797e230237a
MF-lucky <3231424143@qq.com>
2026-05-02 20:23:01 +0800

    fix: remove duplicate backend package structure

--- backend/src/main/java/nz/ac/massey/studentjobboard/application/ApplicationStatusHistoryRepository.java
+++
@@ -1,9 +0,0 @@
-package nz.ac.massey.studentjobboard.application;
-
-import java.util.List;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ApplicationStatusHistoryRepository extends JpaRepository<ApplicationStatusHistory, Long> {
-
-    List<ApplicationStatusHistory> findByApplicationIdOrderByChangedAtAsc(Long applicationId);
-}
--- backend/src/main/java/nz/ac/massey/studentjobboard/profile/StudentProfileRepository.java
+++
@@ -1,9 +0,0 @@
-package nz.ac.massey.studentjobboard.profile;
-
-import java.util.Optional;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface StudentProfileRepository extends JpaRepository<StudentProfile, Long> {
-
-    Optional<StudentProfile> findByStudentId(Long studentId);
-}
